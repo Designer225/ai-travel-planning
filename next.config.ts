e8f2028bf3e3e7ebe0789aa43c3c262389056298
Designer225 <designer225@protonmail.com>
2025-12-11 01:15:47 -0800

    Last cleanup before performance eval

--- next.config.ts
+++ next.config.ts
@@ -2,4 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {};
 
+module.exports = {
+    images: {
+        remotePatterns: [
+            new URL('https://images.unsplash.com/photo-1669986480140-2c90b8edb443?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHx0cmF2ZWwlMjBhZHZlbnR1cmUlMjBtb3VudGFpbnxlbnwxfHx8fDE3NjI5MzUwNjJ8MA&ixlib=rb-4.1.0&q=80&w=1080')
+        ]
+    }
+}
+
 export default nextConfig;
